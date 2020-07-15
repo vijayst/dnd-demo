d6fba494dc7f22e2d9d03929b7c1dae5b5059e11
Vijay Thirugnanam <vijayst@gmail.com>
2020-07-15 19:36:28 +0530

    box component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
 
+function Box() {
+  return <div className="box">Box</div>;
+}
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="app">
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+    </div>
+  );
 }
 
 export default App;

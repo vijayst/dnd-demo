bcf47b95c6d5bb4255dd7120f77d86dfaed9d29c
Vijay Thirugnanam <vijayst@gmail.com>
2020-07-17 13:45:15 +0530

    drag and drop support

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { useDrag, useDrop } from "react-dnd";
 
 function Card() {
-  return <div className="card">Card</div>;
+  const [{ isDragging }, dragRef] = useDrag({
+    item: {
+      type: "card",
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+  return (
+    <div
+      className="card"
+      ref={dragRef}
+      style={{
+        backgroundColor: isDragging ? "#fbb" : "palegoldenrod",
+      }}
+    >
+      Card
+    </div>
+  );
 }
 
-function Box({ card }) {
-  return <div className="box">{card ? <Card /> : "Box"}</div>;
+function Box({ card, moveCard }) {
+  const [{ isOver }, dropRef] = useDrop({
+    accept: "card",
+    drop: () => moveCard(),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  });
+  return (
+    <div
+      className="box"
+      ref={dropRef}
+      style={{ backgroundColor: isOver ? "#bbf" : "rgba(0,0,0,.12" }}
+    >
+      {card ? <Card /> : "Box"}
+    </div>
+  );
 }
 
 function App() {
+  const [index, setIndex] = useState(1);
+
+  function moveCard(i) {
+    setIndex(i);
+  }
+
   return (
     <div className="app">
-      <Box card></Box>
-      <Box></Box>
-      <Box></Box>
+      <Box card={index === 1} moveCard={moveCard.bind(null, 1)}></Box>
+      <Box card={index === 2} moveCard={moveCard.bind(null, 2)}></Box>
+      <Box card={index === 3} moveCard={moveCard.bind(null, 3)}></Box>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
